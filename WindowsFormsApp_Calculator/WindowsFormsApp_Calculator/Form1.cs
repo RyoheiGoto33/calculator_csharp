aade78ff33f83fb6d4b86d73185600c5657c4b26
Ryohei Goto <ryohei.goto@scsk-ahs.co.jp>
2024-12-03 13:56:20 +0900

    フォーム画面のみ実装

---
+++ WindowsFormsApp_Calculator/WindowsFormsApp_Calculator/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Calculator
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// フォームのコンストラクタ
+        /// フォームの起動時に一度だけ呼び出される
+        /// </summary>
+        public Form1()
+        {
+            // フォームの初期化処理
+            InitializeComponent();
+        }
+    }
+}
